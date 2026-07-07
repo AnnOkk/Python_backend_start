333282a5d19f0b0b2762c6b03c36510e48c59d59
AnnOkk <chereshnyasladkaya@gmail.com>
2026-07-07 13:50:26 +0300

    Add `DictCache` implementation and initial scaffolding for `LfuDictCache`
    - Implement `DictCache` class with LRU behavior using `OrderedDict`.
    - Add partially implemented `LfuDictCache` class structure.
    - Include IntelliJ IDEA configuration files for project setup.

---
+++ main.py
@@ -0,0 +1,92 @@
+from collections import OrderedDict
+from typing import Hashable, Generic, Iterator, TypeVar
+from sortedcontainers import SortedDict
+
+K = TypeVar('K', bound=Hashable)
+V = TypeVar("V")
+
+
+####################################################################################
+
+class DictCache(OrderedDict[K, V]):
+    def __init__(self, maxsize=128):
+        super().__init__()  # calls constructor of OrderedDict that has all methods for keeping insertion order
+        self.maxsize = maxsize
+
+    # The  methods __getitem__ and __setitem__ should be overriden
+    # Assumption: only following methods should be overriden for making tests from test_dict_cache.py passed
+    # Hints as follows:
+    # super().__getitem__(key) calls method __getitem__ of OrderedDict
+    # super().__setitem__(key, value) calls method __setitem__ of OrderedDict
+    # consider using self.move_to_end(key) of OrderedDict for making item with the given key as most recent
+    # consider using self.popitem(last=False) for removing least recent (eldest item)
+
+    def __getitem__(self, key) -> V:
+        res = super().__getitem__(key)
+        self.move_to_end(key)
+        return res
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self.move_to_end(key)
+        if len(self) > self.maxsize:
+            self.popitem(last=False)
+
+
+class LfuDictCache(Generic[K, V]):
+    def __init__(self, max_size: int):
+        # TODO write constructor for defining encapsulated data structure
+        self.max_size = max_size
+        self.data = {}
+        self.counter = 0
+        self.frequency = {}  # for each key, how many times it was accessed
+        self.last_used = {}  # for each key, when it was accessed last time
+        self.sorted_keys = SortedDict()
+
+        raise NotImplementedError()
+
+    def __getitem__(self, key: K) -> V:
+        # TODO method for square braces operator [] getting key and returning value with throwing
+        # KeyError exception if key is missing
+        if key not in self.data:
+            raise KeyError()
+        value = self.data[key]
+        self.counter += 1
+        self.frequency[key] += 1
+        self.last_used[key] = self.counter
+        return value
+
+    def __setitem__(self, key: K, value: V):
+        # TODO method for square braces operator [] either updating existing key-value association or adding a new one
+        if key in self.data and len(self.data) < self.max_size:
+            self.data[key] = value
+            self.counter += 1
+            self.frequency[key] += 1
+            self.last_used[key] = self.counter
+        if key not in self.data and len(self.data) < self.max_size:
+            self.data[key] = value
+            self.counter += 1
+            self.frequency[key] = 1
+            self.last_used[key] = self.counter
+        if len(self.data) == self.max_size:
+            for k, v in self.last_used.items():
+                pass
+
+    def __delitem__(self, key: K):
+        # TODO method for deleting key-value association from a dictionary with throwing KeyError exception
+        # in the case of missing key like del dict[key]
+        if key not in self.data:
+            raise KeyError()
+        del self.data[key]
+        del self.frequency[key]
+        del self.last_used[key]
+
+    def __iter__(self) -> Iterator[K]:
+        # TODO method for iterating keys in arbitrary order
+        return iter(self.data)
+
+    # after doing sorted_keys must be return iter(self.sorted_keys)
+
+    def __len__(self) -> int:
+        # TODO method returning number of key-value associations (pairs)
+        return len(self.data)
